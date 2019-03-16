80432e3c058d61318778bbd54038abb4d61d65ee
josuevargassosa <josuevargass@hotmail.com>
2019-03-16 13:12:48 -0500

    [ADD] Error design home image and search

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -11,14 +11,7 @@ export class SettingsPage implements OnInit {
   constructor(public navCtrl: NavController) { }
 
 
-  doRefresh(event) {
-    console.log('Begin async operation');
-
-    setTimeout(() => {
-      console.log('Async operation has ended');
-      event.target.complete();
-    }, 2000);
-  }
+  
 
   ngOnInit() {
   }
